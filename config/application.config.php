3acb77db951d2c363a109b86665a1358c22e99e4
Saulo Soares de Toledo <saulotoledo@gmail.com>
2015-11-20 22:44:55 -0300

    Added the modules 'Market' and 'Search', and installed the 'Zend Developer Tools'.

--- config/application.config.php
+++ config/application.config.php
@@ -8,7 +8,10 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
+        'ZendDeveloperTools',
         'Application',
+        'Market',
+        'Search',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
